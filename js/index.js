773658f27bcb2218f57174398127fdc597ab4fe5
himanshu <himanshu@himanshus-MacBook-Pro.local>
2019-09-22 15:59:27 +0530

    -

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
 (function(){
+	var timelineSwiper = new Swiper ('.timeline .swiper-container', {
+	  direction: 'vertical',
+	  loop: false,
+	  speed: 1600,
+	  pagination: '.swiper-pagination',
+	  paginationBulletRender: function (swiper, index, className) {
+	    var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+	    return '<span class="' + className + '">' + year + '</span>';
+	  },
+	  paginationClickable: true,
+	  nextButton: '.swiper-button-next',
+	  prevButton: '.swiper-button-prev',
+	  breakpoints: {
+	    768: {
+	      direction: 'horizontal',
+	    }
+	  }
+	});
+	
 	var addEvent = function(object, type, callback) {
 	    if (object == null || typeof(object) == 'undefined') return;
 	    if (object.addEventListener) {
@@ -50,3 +69,6 @@
 	detectInput('touchmove');
 	
 })();
+
+
+// Code By Webdevtrick ( https://webdevtrick.com )
